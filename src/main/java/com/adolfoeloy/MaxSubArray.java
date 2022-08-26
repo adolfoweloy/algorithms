a000dff0187e5f7c6e84cfa2041d9f0fcc8d5212
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-08-26 16:37:33 +1000

    solution using kadane's algorithm -- DP

--- src/main/java/com/adolfoeloy/MaxSubArray.java
+++ src/main/java/com/adolfoeloy/MaxSubArray.java
@@ -3,17 +3,12 @@ package com.adolfoeloy;
 public class MaxSubArray {
 
     public int maxSubArray(int[] nums) {
-        int maxSubArray = Integer.MIN_VALUE;
-        for (int i=0; i < nums.length; i++) {
-            int currentSubArray = 0;
-            for (int j = i; j < nums.length; j++) {
-                // adding the number at each iteration and creating comparing it at each iteration
-                // is the trick to avoid N^3. With this trick there's no need for a third nested for.
-                currentSubArray += nums[j];
-                maxSubArray = Integer.max(maxSubArray, currentSubArray);
-            }
+        int currentSubarray = nums[0];
+        int maxSubArray = nums[0];
+        for (int i=1; i<nums.length; i++) {
+            currentSubarray = (currentSubarray < 0) ? nums[i] : currentSubarray + nums[i];
+            maxSubArray = Integer.max(currentSubarray, maxSubArray);
         }
-
         return maxSubArray;
     }
 
