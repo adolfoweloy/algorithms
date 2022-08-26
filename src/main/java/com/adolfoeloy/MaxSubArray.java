4f03f4b26646839f7bf3f8770825416694970f80
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-08-26 19:09:29 +1000

    fixes bug introduced which works for the variation of maxsubarray when negative numbers are not considered valid max values

--- src/main/java/com/adolfoeloy/MaxSubArray.java
+++ src/main/java/com/adolfoeloy/MaxSubArray.java
@@ -3,9 +3,9 @@ package com.adolfoeloy;
 public class MaxSubArray {
 
     public int maxSubArray(int[] nums) {
-        int ans = nums[0];  // ans stands for answer (max subarray)
-        int a = ans;        // a is the current subarray sum
-        for (int i : nums) {
+        int ans = nums[0]; // ans stands for answer (max subarray)
+        int a = nums[0];   // a is the current subarray sum
+        for (int i = 1; i <  nums.length; i++) {
             int n = nums[i];
             a = Integer.max(n, n + a);
             ans = Integer.max(a, ans);
