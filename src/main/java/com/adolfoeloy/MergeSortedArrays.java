91bc459ddff0a07a28ae7bbf3db5b9171340bea8
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-08-27 18:26:47 +1000

    merge sorted arrays using three pointers and copy of first array

--- src/main/java/com/adolfoeloy/MergeSortedArrays.java
+++ src/main/java/com/adolfoeloy/MergeSortedArrays.java
@@ -3,12 +3,15 @@ package com.adolfoeloy;
 import java.util.Arrays;
 
 public class MergeSortedArrays {
-    public void merge(int[] a, int n, int[] b, int m) {
-        int j = 0;
-        for (int i=n; i < a.length; i++) {
-            a[i] = b[j];
-            j = j + 1;
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] cp = Arrays.copyOf(nums1, m);
+        int p1 = 0, p2 = 0;
+        for (int p=0; p<(m+n); p++) {
+            if (p2 >= n || (p1 < m && cp[p1] <= nums2[p2])) {
+                nums1[p] = cp[p1++];
+            } else {
+                nums1[p] = nums2[p2++];
+            }
         }
-        Arrays.sort(a);
     }
 }
