9ec5fea85118f3362d03ff4eec6be960841278ce
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-09-03 18:02:24 +1000

    using amortised O(1) time complexity for pop operations. Push is done O(1)

--- src/main/java/com/adolfoeloy/datastructure/QueueWithStack.java
+++ src/main/java/com/adolfoeloy/datastructure/QueueWithStack.java
@@ -6,28 +6,36 @@ import java.util.Stack;
  * This is an implementation of a queue using stacks.
  */
 public class QueueWithStack {
-    private final Stack<Integer> stack = new Stack<>();
-    private final Stack<Integer> tmp = new Stack<>();
+    private final Stack<Integer> s1 = new Stack<>();
+    private final Stack<Integer> s2 = new Stack<>();
+    private Integer front;
 
     public void push(int x) {
-        while (!stack.isEmpty()) {
-            tmp.push(stack.pop());
+        if (s1.isEmpty()) {
+            front = x;
         }
-        stack.push(x);
-        while (!tmp.isEmpty()) {
-            stack.push(tmp.pop());
-        }
+        s1.push(x);
     }
 
     public int pop() {
-        return stack.pop();
+        if (!s2.isEmpty()) {
+            return s2.pop();
+        }
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+        return s2.pop();
     }
 
     public int peek() {
-        return stack.peek();
+        if (empty()) throw new IllegalStateException("The queue is empty");
+        if (!s2.empty()) {
+            return s2.peek();
+        }
+        return front;
     }
 
     public boolean empty() {
-        return stack.isEmpty();
+        return s1.empty() && s2.empty();
     }
 }
