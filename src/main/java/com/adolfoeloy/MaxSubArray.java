4fe10e4d94457ca3afea8ba5fc37d9499fcb1f2d
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-09-03 16:12:12 +1000

    simplifies max sub array

--- src/main/java/com/adolfoeloy/MaxSubArray.java
+++ src/main/java/com/adolfoeloy/MaxSubArray.java
@@ -3,10 +3,9 @@ package com.adolfoeloy;
 public class MaxSubArray {
 
     public int maxSubArray(int[] nums) {
-        int ans = nums[0]; // ans stands for answer (max subarray)
-        int a = nums[0];   // a is the current subarray sum
-        for (int i = 1; i <  nums.length; i++) {
-            int n = nums[i];
+        int ans = Integer.MIN_VALUE;
+        int a = 0;
+        for (int n : nums) {
             a = Integer.max(n, n + a);
             ans = Integer.max(a, ans);
         }
