e22715c26e59bd1499f137771966cb998962ac62
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-09-05 20:16:57 +1000

    my own solution to intersection of two arrays

--- src/main/java/com/adolfoeloy/IntersectionOfTwoArrays.java
+++ src/main/java/com/adolfoeloy/IntersectionOfTwoArrays.java
@@ -4,21 +4,19 @@ import java.util.*;
 
 public class IntersectionOfTwoArrays {
     public int[] intersect(int[] nums1, int[] nums2) {
-        if (nums1.length < nums2.length) {
-            return intersect(nums2, nums1);
+        int[] r = new int[Integer.min(nums1.length, nums2.length)];
+        Map<Integer, Integer> h = new HashMap<>();
+        for (int n : nums1) {
+            h.put(n, h.getOrDefault(n, 0) + 1);
         }
-        Map<Integer, Integer> hash = new HashMap<>();
-        for (int n:nums1) {
-            hash.put(n, hash.getOrDefault(n, 0) + 1);
-        }
-        int k=0;
-        for (int n:nums2) {
-            int v = hash.getOrDefault(n, 0);
+        int count = 0;
+        for (int n : nums2) {
+            int v = h.getOrDefault(n, 0);
             if (v > 0) {
-                nums1[k++] = n;
-                hash.put(n, v-1);
+                r[count++] = n;
+                h.put(n, v-1);
             }
         }
-        return Arrays.copyOfRange(nums1, 0, k);
+        return Arrays.copyOf(r, count);
     }
 }
