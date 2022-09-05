ff298ca8fa4fe24207d62f6ac1eaf7b71460227a
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2022-09-05 23:47:43 +1000

    DP: house robber problem using top-down approach

---
+++ src/test/java/com/adolfoeloy/dpstudy/HouseRobberRecursiveTest.java
@@ -0,0 +1,24 @@
+package com.adolfoeloy.dpstudy;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class HouseRobberRecursiveTest {
+    private final HouseRobberRecursive subject = new HouseRobberRecursive();
+
+    @Test
+    void testCase1() {
+        assertThat(subject.rob(new int[] {1,2,3,1})).isEqualTo(4);
+    }
+
+    @Test
+    void testCase2() {
+        assertThat(subject.rob(new int[] {2,7,9,3,1})).isEqualTo(12);
+    }
+
+    @Test
+    void testCase3() {
+        assertThat(subject.rob(new int[] {0})).isEqualTo(0);
+    }
+}
