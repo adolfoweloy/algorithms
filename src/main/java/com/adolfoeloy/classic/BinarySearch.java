42d72862f02f19b0edd3479fd1b163483a62e3b3
Adolfo Eloy <1496547+adolfoweloy@users.noreply.github.com>
2024-02-18 17:47:59 +1100

    change my binary search from recursive to iterative

--- src/main/java/com/adolfoeloy/classic/BinarySearch.java
+++ src/main/java/com/adolfoeloy/classic/BinarySearch.java
@@ -2,16 +2,14 @@ package com.adolfoeloy.classic;
 
 public class BinarySearch {
     int find(int[] a, int n) {
-        return _find(a, 0, a.length-1, n);
-    }
-
-    int _find(int[] a, int low, int high, int n) {
-        if (low > high) return -1;
-        int mid = low + (high - low) / 2;
-
-        if (a[mid] < n) return _find(a, mid+1, high, n);
-        if (a[mid] > n) return _find(a, low, mid-1, n);
-
-        return mid;
+        var lo = 0;
+        var hi = a.length - 1;
+        while (lo <= hi) {
+            var mid = lo + (hi - lo) / 2;
+            if (n < a[mid]) hi = mid - 1;
+            else if (n > a[mid]) lo = mid + 1;
+            else return mid;
+        }
+        return -1;
     }
 }
