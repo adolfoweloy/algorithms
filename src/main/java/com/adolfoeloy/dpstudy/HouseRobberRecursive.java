ff55e83821417bac8c990b67a674a02822edc2d0
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2024-10-20 10:45:31 +1100

    Refactoring recursive house robber from using map to simple array

--- src/main/java/com/adolfoeloy/dpstudy/HouseRobberRecursive.java
+++ src/main/java/com/adolfoeloy/dpstudy/HouseRobberRecursive.java
@@ -1,24 +1,24 @@
 package com.adolfoeloy.dpstudy;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
 
 public class HouseRobberRecursive {
 
     public int rob(int[] nums) {
-        Map<Integer, Integer> memo = new HashMap<>();
+        int[] memo = new int[nums.length];
+        Arrays.fill(memo, -1);
         return dp(memo, nums, nums.length - 1);
     }
 
-    private int dp(Map<Integer, Integer> memo, int[] nums, int idx) {
+    private int dp(int[] memo, int[] nums, int idx) {
         if (idx == 0) return nums[0];
         if (idx == 1) return Integer.max(nums[0], nums[1]);
 
-        if (!memo.containsKey(idx))
-            memo.put(idx, Integer.max(
+        if (memo[idx] == -1)
+            memo[idx] = Integer.max(
                     dp(memo, nums, idx - 1),                    // dp(i - 1)
-                    dp(memo, nums, idx - 2) + nums[idx]));      // dp(i - 2) + n[i]
+                    dp(memo, nums, idx - 2) + nums[idx]);      // dp(i - 2) + n[i]
 
-        return memo.get(idx);
+        return memo[idx];
     }
 }
