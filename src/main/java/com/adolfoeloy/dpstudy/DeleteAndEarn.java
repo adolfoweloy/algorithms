691a9850bc4bf17a77d173cb4246f2a1577cb3f5
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2024-10-20 15:10:18 +1100

    Studying DP - delete and earn problem

--- src/main/java/com/adolfoeloy/dpstudy/DeleteAndEarn.java
+++ src/main/java/com/adolfoeloy/dpstudy/DeleteAndEarn.java
@@ -1,9 +1,37 @@
 package com.adolfoeloy.dpstudy;
 
-public class DeleteAndEarn {
+import java.util.HashMap;
+import java.util.Map;
 
-    public int solve(int[] nums) {
-        return 0;
+public class DeleteAndEarn {
+    Map<Integer, Integer> points = new HashMap<>();
+    Map<Integer, Integer> memo = new HashMap<>();
+
+    public int deleteAndEarn(int[] nums) {
+        // use the max because this way I don't need to worry about deleting (n+1)
+        // I am always looking backwards to smaller numbers if starting from max (there's nothing beyond)
+        int max = Integer.MIN_VALUE;
+        for (int n : nums) {
+            if (n > max) max = n;
+            points.put(n, points.getOrDefault(n, 0) + 1);
+        }
+        return dp(max);
     }
+
+    private int dp(int n) {
+        if (n == 0) return 0;
+        if (n == 1) return points.getOrDefault(1, 0);
+
+        if (!memo.containsKey(n)) {
+            memo.put(n, Math.max(
+                dp(n - 2) + (n * points.getOrDefault(n, 0)), // if taking, get the points and look for n-2 (n-1 can't be taken now)
+                dp(n - 1)
+            ));
+
+        }
+
+        return memo.get(n);
+    }
+
 
 }
--- src/test/java/com/adolfoeloy/dpstudy/DeleteAndEarnTest.java
+++ src/test/java/com/adolfoeloy/dpstudy/DeleteAndEarnTest.java
@@ -3,21 +3,24 @@ package com.adolfoeloy.dpstudy;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class DeleteAndEarnTest {
     private DeleteAndEarn subject = new DeleteAndEarn();
 
     @Test
     void test1() {
-        int output = subject.solve(new int[] {3,4,2});
+        int output = subject.deleteAndEarn(new int[] {3,4,2});
         Assertions.assertThat(output).isEqualTo(6);
     }
 
     @Test
-    void test1() {
-        int output = subject.solve(new int[] {2,2,3,3,3,4});
+    void test2() {
+        int output = subject.deleteAndEarn(new int[] {2,2,3,3,3,4});
         Assertions.assertThat(output).isEqualTo(9);
+    }    @Test
+
+    void test3() {
+        int output = subject.deleteAndEarn(new int[] {1,1,1,2,4,5,5,5,6});
+        Assertions.assertThat(output).isEqualTo(18);
     }
 
 }
