904094716ef3eea01d1249352f7fb845a7d4ac82
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2024-11-04 14:27:54 +1100

    Add some graph-processing code

--- src/main/java/com/adolfoeloy/graph/Graph.java
+++ src/main/java/com/adolfoeloy/graph/Graph.java
@@ -1,8 +1,11 @@
 package com.adolfoeloy.graph;
 
 public class Graph {
-    Graph(int vertices) {
+    private int vertices;
+    private int edges;
 
+    Graph(int vertices) {
+        this.vertices = vertices;
     }
 
     /**
@@ -29,14 +32,46 @@ public class Graph {
     /**
      * Vertices adjacent to v.
      */
-    Iterable<Integer> adjacency(int v) {
+    Iterable<Integer> adjacentVertices(int v) {
         return null;
     }
 
+    /**
+     * Compute the degree of vertex v.
+     */
+    public static int degree(Graph G, int v) {
+        int degree = 0;
+        for (int w : G.adjacentVertices(v)) degree++;
+        return degree;
+    }
+
+    /**
+     * Compute the vertex with most adjacent vertices and return its degree.
+     */
+    public static int maxDegree(Graph G) {
+        int max = 0;
+        for (int v = 0; v < G.vertices(); v++) {
+            max = Math.max(degree(G, v), max);
+        }
+        return max;
+    }
+
+    public static int avgDegree(Graph G) {
+        return 2 * G.edges() / G.vertices();
+    }
+
+
     @Override
     public String toString() {
-        final StringBuffer sb = new StringBuffer("Graph{");
-        sb.append('}');
+        StringBuilder sb = new StringBuilder();
+        sb.append(String.format("%d vertices, %d edges\n", vertices, edges));
+        for (int v = 0; v < vertices; v++) {
+            sb.append(v); sb.append(": ");
+            for (int w: this.adjacentVertices(v)) {
+                sb.append(w); sb.append(" ");
+            }
+            sb.append("\n");
+        }
         return sb.toString();
     }
 }
