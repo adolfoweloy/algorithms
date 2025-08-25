4a82792570a533cd6de86dc589339ae960da2597
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-08-25 20:43:38 +1000

    Update permutation test

--- src/test/java/com/adolfoeloy/classic/PermutationTest.java
+++ src/test/java/com/adolfoeloy/classic/PermutationTest.java
@@ -3,6 +3,12 @@ package com.adolfoeloy.classic;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 class PermutationTest {
 
     private Permutation subject = new Permutation();
@@ -24,5 +30,13 @@ class PermutationTest {
         Assertions.assertThat(subject.permut("abc")).contains(
                 "abc", "bac", "cba", "acb", "bca", "cab"
         );
+    }
+
+
+    @Test
+    void testArrayList() {
+        int[] nums = {1, 2, 3};
+        var result = IntStream.of(nums).boxed().toList();
+        Assertions.assertThat(result).containsExactly(1, 2, 3);
     }
 }
