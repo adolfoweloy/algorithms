68e99f06ea77450a270012fc40a6637ff04b8a8c
adolfoweloy <1496547+adolfoweloy@users.noreply.github.com>
2025-08-28 10:07:09 +1000

    Reimplemented LRU cache

--- src/main/java/com/adolfoeloy/hashtable/DefaultLRUCache.java
+++ src/main/java/com/adolfoeloy/hashtable/DefaultLRUCache.java
@@ -1,86 +1,134 @@
 package com.adolfoeloy.hashtable;
 
-import com.google.common.annotations.VisibleForTesting;
-
 import java.util.HashMap;
 import java.util.Map;
 
 public class DefaultLRUCache implements LRUCache {
     private final int capacity;
-    private final Map<Integer, Node> cache = new HashMap<>();
-    private final Node head = new Node();
-    private final Node tail = new Node();
-
-    private static class Node {
-        Integer key;
-        Integer value;
-        Node next;
-        Node prev;
-        Node(Integer key, Integer value) {
-            this.key = key;
-            this.value = value;
-        }
-        Node() {}
-    }
+    private final Map<Integer, Node> cache;
+    private final DoublyLinkedList usageList;
 
     public DefaultLRUCache(int capacity) {
         this.capacity = capacity;
-        head.next = tail;
-        tail.prev = head;
+        this.cache = new HashMap<>(capacity);
+        this.usageList = new DoublyLinkedList();
     }
 
     @Override
     public int get(int key) {
-        Node node = cache.get(key);
-        if (node == null) return -1;
-        return moveHead(node).value;
+        var element = cache.get(key);
+
+        if (element == null) {
+            return -1;
+        }
+
+        // update the usage list
+        usageList.moveToFront(element);
+
+        return element.value;
     }
 
     @Override
     public void put(int key, int value) {
-        Node node = cache.get(key);
-        if (node != null) {
-            moveHead(node);
-            node.value = value;
-        } else {
-            Node newNode = new Node(key, value);
-            cache.put(key, newNode);
-            addFront(newNode);
+        var element = cache.get(key);
+
+        if (element == null) {
+            var node = usageList.addFirst(key, value);
+            cache.put(key, node);
             if (cache.size() > capacity) {
-                Node removed = removeTail();
+                var removed = usageList.removeLast();
                 cache.remove(removed.key);
             }
+        } else {
+            usageList.moveToFront(element);
         }
     }
 
-    private void addFront(Node node) {
-        head.next.prev = node;
-        node.next = head.next;
-        node.prev = head;
-        head.next = node;
-    }
-
-    private Node moveHead(Node node) {
-        remove(node);
-        addFront(node);
-        return node;
-    }
-
-    private Node removeTail() {
-        Node toRemove = tail.prev;
-        remove(toRemove);
-        return toRemove;
-    }
-
-    private void remove(Node node) {
-        Node next = node.next;
-        Node prev = node.prev;
-        next.prev = prev;
-        prev.next = next;
-    }
-
-    @VisibleForTesting
+    @Override
     public int size() {
-         return cache.size();
+        return cache.size();
+    }
+
+    static class Node {
+        private Node previous;
+        private Node next;
+        private int key;
+        private int value;
+
+        Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private static class DoublyLinkedList {
+        private Node head;
+        private Node tail;
+
+        DoublyLinkedList() {
+            head = new Node(-1, -1);
+            tail = new Node(-1, -1);
+
+            head.next = tail;
+            tail.previous = head;
+        }
+
+        public void moveToFront(Node element) {
+            var prev = element.previous;
+            var next = element.next;
+
+            prev.next = next;
+            next.previous = prev;
+
+            addFirst(element);
+        }
+
+        public Node addFirst(int key, int value) {
+            return addFirst(new Node(key, value));
+        }
+
+        private Node addFirst(Node node) {
+            // start the logic
+            // node pointers
+            node.next = head.next;
+            node.previous = head;
+
+            // pointing to the node
+            head.next.previous = node;
+            head.next = node;
+
+            return node;
+        }
+
+        public Node removeLast() {
+            if (head.next == tail.next) {
+                throw new IllegalStateException("The list is empty. There is nothing to remove.");
+            }
+
+            var removed = tail.previous;
+            removed.previous.next = tail;
+            tail.previous = removed.previous;
+
+            // avoid loitering
+            removed.next = null;
+            removed.previous = null;
+
+            return removed;
+        }
+
+        String printList() {
+            StringBuilder sb = new StringBuilder();
+            Node h = head;
+            sb.append("head");
+            while (h != tail) {
+                if (h != head) {
+                    sb.append("->");
+                    sb.append(h.value);
+                }
+                h = h.next;
+            }
+            sb.append("->tail");
+            return sb.toString();
+        }
     }
 }
